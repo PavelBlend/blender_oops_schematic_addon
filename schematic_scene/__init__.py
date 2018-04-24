cc707f32ced21172206fb85c31a36b26c3f01cb8
PavelBlend <stalkermodkytia@yandex.ru>
2018-04-24 22:03:50 +0400

    variables for node sizes

--- schematic_scene/__init__.py
+++ schematic_scene/__init__.py
@@ -23,6 +23,9 @@ from bpy.types import NodeTree
 # font_id = blf.load(font_path)
 font_id = 0
 char_size = 8
+y_distance = 100
+x_distance = 25
+node_hight = 25
 
 
 class SceneNodeTree(NodeTree):
@@ -52,24 +55,24 @@ class SchematicNode:
 
             for child in self.children:
                 bgl.glBegin(bgl.GL_LINES)
-                bgl.glVertex2f(self.offset_x + len(self.text) * (char_size / 2), (self.layer) * 200 + 50)
-                bgl.glVertex2f(child.offset_x + len(child.text) * (char_size / 2), (self.layer) * 200 + 200)
+                bgl.glVertex2f(self.offset_x + len(self.text) * (char_size / 2), (self.layer) * y_distance + node_hight)
+                bgl.glVertex2f(child.offset_x + len(child.text) * (char_size / 2), (self.layer) * y_distance + y_distance)
                 bgl.glEnd()
 
         def _draw_box():
             bgl.glColor3f(*self.color)
 
             bgl.glBegin(bgl.GL_QUADS)
-            bgl.glVertex2f(self.offset_x, self.layer * 200)
-            bgl.glVertex2f(self.offset_x, self.layer * 200 + 50)
-            bgl.glVertex2f(len(self.text) * char_size + self.offset_x, self.layer * 200 + 50)
-            bgl.glVertex2f(len(self.text) * char_size + self.offset_x, self.layer * 200)
+            bgl.glVertex2f(self.offset_x, self.layer * y_distance)
+            bgl.glVertex2f(self.offset_x, self.layer * y_distance + node_hight)
+            bgl.glVertex2f(len(self.text) * char_size + self.offset_x, self.layer * y_distance + node_hight)
+            bgl.glVertex2f(len(self.text) * char_size + self.offset_x, self.layer * y_distance)
             bgl.glEnd()
 
         def _draw_text():
             bgl.glColor3f(0, 0, 0)
 
-            blf.position(font_id, self.offset_x, self.layer * 200 + 16, 0)
+            blf.position(font_id, self.offset_x, self.layer * y_distance + node_hight / 4, 0)
             blf.blur(font_id, 0)
             blf.size(font_id, 30, 30)
             blf.draw(font_id, self.text)
@@ -92,21 +95,21 @@ def draw_scene_nodes():
                 last_offset = 0
                 for mesh_index, mesh in enumerate(bpy.data.meshes):
                     mesh_node = SchematicNode(0, 0, 0, 0, mesh.name, (0.6, 0.6, 0.6), mesh_index, 2, last_offset)
-                    last_offset += len(mesh.name) * char_size + 50
+                    last_offset += len(mesh.name) * char_size + x_distance
                     meshes_nodes[mesh.name] = mesh_node
                     schematic_nodes.append(mesh_node)
 
                 last_offset = 0
                 for scene_index, scene in enumerate(bpy.data.scenes):
                     scene_node = SchematicNode(0, 0, 0, 0, scene.name, (0.2, 0.4, 0.8), scene_index, 0, last_offset)
-                    last_offset += len(scene.name) * char_size + 50
+                    last_offset += len(scene.name) * char_size + x_distance
                     scene_nodes[scene.name] = scene_node
                     schematic_nodes.append(scene_node)
 
                 last_offset = 0
                 for object_index, object in enumerate(bpy.data.objects):
                     object_node = SchematicNode(0, 0, 0, 0, object.name, (0.8, 0.4, 0.2), object_index, 1, last_offset)
-                    last_offset += len(object.name) * char_size + 50
+                    last_offset += len(object.name) * char_size + x_distance
                     if object.type == 'MESH':
                         mesh_node = meshes_nodes[object.data.name]
                         mesh_node.parents.append(object_node)
